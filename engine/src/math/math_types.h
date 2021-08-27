4375b3e9b716ef013929bb5fb28a48454c2215af
Travis Vroman <travisvroman@gmail.com>
2021-08-27 14:02:15 -0400

    Fixed vec3 definition

--- engine/src/math/math_types.h
+++ engine/src/math/math_types.h
@@ -17,23 +17,21 @@ typedef union vec2_u {
     };
 } vec2;
 
-typedef struct vec3_u {
-    union {
-        // An array of x, y, z
-        f32 elements[3];
-        struct {
-            union {
-                // The first element.
-                f32 x, r, s, u;
-            };
-            union {
-                // The second element.
-                f32 y, g, t, v;
-            };
-            union {
-                // The third element.
-                f32 z, b, p, w;
-            };
+typedef union vec3_u {
+    // An array of x, y, z
+    f32 elements[3];
+    struct {
+        union {
+            // The first element.
+            f32 x, r, s, u;
+        };
+        union {
+            // The second element.
+            f32 y, g, t, v;
+        };
+        union {
+            // The third element.
+            f32 z, b, p, w;
         };
     };
 } vec3;
@@ -68,4 +66,3 @@ typedef union vec4_u {
 } vec4;
 
 typedef vec4 quat;
-
