f53cd506f84bdaebe92623546adee1cdfc2feb3c
Travis Vroman <travisvroman@gmail.com>
2021-09-10 14:44:25 -0400

    Removed free of pointer that shouldn't have been freed

--- engine/src/renderer/vulkan/vulkan_shader_utils.c
+++ engine/src/renderer/vulkan/vulkan_shader_utils.c
@@ -53,10 +53,5 @@ b8 create_shader_module(
     shader_stages[stage_index].shader_stage_create_info.module = shader_stages[stage_index].handle;
     shader_stages[stage_index].shader_stage_create_info.pName = "main";
 
-    if (file_buffer) {
-        kfree(file_buffer, sizeof(u8) * size, MEMORY_TAG_STRING);
-        file_buffer = 0;
-    }
-
     return true;
 }
