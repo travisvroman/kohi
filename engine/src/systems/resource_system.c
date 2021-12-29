9f2d31585d0a666c808ec9664bb1acacdfb20979
Travis Vroman <travisvroman@gmail.com>
2021-12-29 10:40:26 -0500

    Fixed null dereference in resource loader.

--- engine/src/systems/resource_system.c
+++ engine/src/systems/resource_system.c
@@ -144,7 +144,9 @@ const char* resource_system_base_path() {
 
 b8 load(const char* name, resource_loader* loader, resource* out_resource) {
     if (!name || !loader || !loader->load || !out_resource) {
-        out_resource->loader_id = INVALID_ID;
+        if (out_resource) {
+            out_resource->loader_id = INVALID_ID;
+        }
         return false;
     }
 
