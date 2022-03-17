92ab510f9bc14bbf579b8deefc6e30333edf8acf
Travis Vroman <travisvroman@gmail.com>
2022-03-17 19:58:06 -0400

    Default normal map fix

--- engine/src/systems/texture_system.c
+++ engine/src/systems/texture_system.c
@@ -287,6 +287,8 @@ b8 create_default_textures(texture_system_state* state) {
             u64 index = (row * 16) + col;
             u64 index_bpp = index * channels;
             // Set blue, z-axis by default and alpha.
+            normal_pixels[index_bpp + 0] = 128;
+            normal_pixels[index_bpp + 1] = 128;
             normal_pixels[index_bpp + 2] = 255;
             normal_pixels[index_bpp + 3] = 255;
         }
