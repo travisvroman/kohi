043ccbd2ed4ff6283eaf7b907c70bdab28932f54
Travis Vroman <travisvroman@gmail.com>
2022-05-21 13:38:48 -0400

    060 Camera System

---
+++ engine/src/renderer/camera.h
@@ -0,0 +1,198 @@
+/**
+ * @file camera.h
+ * @author Travis Vroman (travis@kohiengine.com)
+ * @brief 
+ * @version 1.0
+ * @date 2022-05-21
+ * 
+ * @copyright Kohi Game Engine is Copyright (c) Travis Vroman 2021-2022
+ * 
+ */
+
+#pragma once
+
+#include "math/math_types.h"
+
+/**
+ * @brief Represents a camera that can be used for
+ * a variety of things, especially rendering. Ideally,
+ * these are created and managed by the camera system.
+ */
+typedef struct camera {
+    /**
+     * @brief The position of this camera.
+     * NOTE: Do not set this directly, use camera_positon_set() instead
+     * so the view matrix is recalculated when needed.
+     */
+    vec3 position;
+    /**
+     * @brief The rotation of this camera using Euler angles (pitch, yaw, roll).
+     * NOTE: Do not set this directly, use camera_rotation_euler_set() instead
+     * so the view matrix is recalculated when needed.
+     */
+    vec3 euler_rotation;
+    /** @brief Internal flag used to determine when the view matrix needs to be rebuilt. */
+    b8 is_dirty;
+
+    /**
+     * @brief The view matrix of this camera.
+     * NOTE: IMPORTANT: Do not get this directly, use camera_view_get() instead
+     * so the view matrix is recalculated when needed.
+     */
+    mat4 view_matrix;
+} camera;
+
+/**
+ * @brief Creates a new camera with default zero position
+ * and rotation, and view identity matrix. Ideally, the
+ * camera system should be used to create this instead
+ * of doing so directly.
+ *
+ * @return A copy of a newly-created camera.
+ */
+camera camera_create();
+
+/**
+ * @brief Defaults the provided camera to default zero
+ * rotation and position, and view matrix to identity.
+ *
+ * @param c A pointer to the camera to be reset.
+ */
+void camera_reset(camera* c);
+
+/**
+ * @brief Gets a copy of the camera's position.
+ *
+ * @param c A constant pointer to a camera.
+ * @return A copy of the camera's position.
+ */
+vec3 camera_position_get(const camera* c);
+
+/**
+ * @brief Sets the provided camera's position.
+ *
+ * @param c A pointer to a camera.
+ * @param position The position to be set.
+ */
+void camera_position_set(camera* c, vec3 position);
+
+/**
+ * @brief Gets a copy of the camera's rotation in Euler angles.
+ *
+ * @param c A constant pointer to a camera.
+ * @return A copy of the camera's rotation in Euler angles.
+ */
+vec3 camera_rotation_euler_get(const camera* c);
+
+/**
+ * @brief Sets the provided camera's rotation in Euler angles.
+ *
+ * @param c A pointer to a camera.
+ * @param position The rotation in Euler angles to be set.
+ */
+void camera_rotation_euler_set(camera* c, vec3 rotation);
+
+/**
+ * @brief Obtains a copy of the camera's view matrix. If camera is
+ * dirty, a new one is created, set and returned.
+ *
+ * @param c A pointer to a camera.
+ * @return A copy of the up-to-date view matrix.
+ */
+mat4 camera_view_get(camera* c);
+
+/**
+ * @brief Returns a copy of the camera's forward vector.
+ *
+ * @param c A pointer to a camera.
+ * @return A copy of the camera's forward vector.
+ */
+vec3 camera_forward(camera* c);
+
+/**
+ * @brief Returns a copy of the camera's backward vector.
+ *
+ * @param c A pointer to a camera.
+ * @return A copy of the camera's backward vector.
+ */
+vec3 camera_backward(camera* c);
+
+/**
+ * @brief Returns a copy of the camera's left vector.
+ *
+ * @param c A pointer to a camera.
+ * @return A copy of the camera's left vector.
+ */
+vec3 camera_left(camera* c);
+
+/**
+ * @brief Returns a copy of the camera's right vector.
+ *
+ * @param c A pointer to a camera.
+ * @return A copy of the camera's right vector.
+ */
+vec3 camera_right(camera* c);
+
+/**
+ * @brief Moves the camera forward by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to move.
+ */
+void camera_move_forward(camera* c, f32 amount);
+
+/**
+ * @brief Moves the camera backward by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to move.
+ */
+void camera_move_backward(camera* c, f32 amount);
+
+/**
+ * @brief Moves the camera left by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to move.
+ */
+void camera_move_left(camera* c, f32 amount);
+
+/**
+ * @brief Moves the camera right by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to move.
+ */
+void camera_move_right(camera* c, f32 amount);
+
+/**
+ * @brief Moves the camera up (straight along the y-axis) by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to move.
+ */
+void camera_move_up(camera* c, f32 amount);
+
+/**
+ * @brief Moves the camera down (straight along the y-axis) by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to move.
+ */
+void camera_move_down(camera* c, f32 amount);
+
+/**
+ * @brief Adjusts the camera's yaw by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to adjust by.
+ */
+void camera_yaw(camera* c, f32 amount);
+
+/**
+ * @brief Adjusts the camera's pitch by the given amount.
+ *
+ * @param c A pointer to a camera.
+ * @param amount The amount to adjust by.
+ */
+void camera_pitch(camera* c, f32 amount);
---
+++ engine/src/systems/camera_system.c
@@ -0,0 +1,158 @@
+#include "camera_system.h"
+
+#include "core/logger.h"
+#include "core/kstring.h"
+#include "containers/hashtable.h"
+#include "renderer/camera.h"
+
+typedef struct camera_lookup {
+    u16 id;
+    u16 reference_count;
+    camera c;
+} camera_lookup;
+
+typedef struct camera_system_state {
+    camera_system_config config;
+    hashtable lookup;
+    void* hashtable_block;
+    camera_lookup* cameras;
+
+    // A default, non-registered camera that always exists as a fallback.
+    camera default_camera;
+} camera_system_state;
+
+static camera_system_state* state_ptr;
+
+b8 camera_system_initialize(u64* memory_requirement, void* state, camera_system_config config) {
+    if (config.max_camera_count == 0) {
+        KFATAL("camera_system_initialize - config.max_camera_count must be > 0.");
+        return false;
+    }
+
+    // Block of memory will contain state structure, then block for array, then block for hashtable.
+    u64 struct_requirement = sizeof(camera_system_state);
+    u64 array_requirement = sizeof(camera_lookup) * config.max_camera_count;
+    u64 hashtable_requirement = sizeof(camera_lookup) * config.max_camera_count;
+    *memory_requirement = struct_requirement + array_requirement + hashtable_requirement;
+
+    if (!state) {
+        return true;
+    }
+
+    state_ptr = (camera_system_state*)state;
+    state_ptr->config = config;
+
+    // The array block is after the state. Already allocated, so just set the pointer.
+    void* array_block = state + struct_requirement;
+    state_ptr->cameras = array_block;
+
+    // Hashtable block is after array.
+    void* hashtable_block = array_block + array_requirement;
+
+    // Create a hashtable for camera lookups.
+    hashtable_create(sizeof(u16), config.max_camera_count, hashtable_block, false, &state_ptr->lookup);
+
+    // Fill the hashtable with invalid references to use as a default.
+    u16 invalid_id = INVALID_ID_U16;
+    hashtable_fill(&state_ptr->lookup, &invalid_id);
+
+    // Invalidate all cameras in the array.
+    u32 count = state_ptr->config.max_camera_count;
+    for (u32 i = 0; i < count; ++i) {
+        state_ptr->cameras[i].id = INVALID_ID_U16;
+        state_ptr->cameras[i].reference_count = 0;
+    }
+
+    // Setup default camera.
+    state_ptr->default_camera = camera_create();
+
+    return true;
+}
+
+void camera_system_shutdown(void* state) {
+    camera_system_state* s = (camera_system_state*)state;
+    if (s) {
+        // NOTE: If cameras need to be destroyed, do it here.
+        // // Invalidate all cameras in the array.
+        // u32 count = s->config.max_camera_count;
+        // for (u32 i = 0; i < count; ++i) {
+        //     if (s->cameras[i].id != INVALID_ID) {
+
+        //     }
+        // }
+    }
+
+    state_ptr = 0;
+}
+
+camera* camera_system_acquire(const char* name) {
+    if (state_ptr) {
+        if (strings_equali(name, DEFAULT_CAMERA_NAME)) {
+            return &state_ptr->default_camera;
+        }
+        u16 id = INVALID_ID_U16;
+        if (!hashtable_get(&state_ptr->lookup, name, &id)) {
+            KERROR("camera_system_acquire failed lookup. Null returned.");
+            return 0;
+        }
+
+        if (id == INVALID_ID_U16) {
+            // Find free slot
+            for (u16 i = 0; i < state_ptr->config.max_camera_count; ++i) {
+                if (i == INVALID_ID_U16) {
+                    id = i;
+                    break;
+                }
+            }
+            if (id == INVALID_ID_U16) {
+                KERROR("camera_system_acquire failed to acquire new slot. Adjust camera system config to allow more. Null returned.");
+                return 0;
+            }
+
+            // Create/register the new camera.
+            KTRACE("Creating new camera named '%s'...");
+            state_ptr->cameras[id].c = camera_create();
+            state_ptr->cameras[id].id = id;
+
+            // Update the hashtable.
+            hashtable_set(&state_ptr->lookup, name, &id);
+        }
+        state_ptr->cameras[id].reference_count++;
+        return &state_ptr->cameras[id].c;
+    }
+
+    KERROR("camera_system_acquire called before system initialzation. Null returned.");
+    return 0;
+}
+
+void camera_system_release(const char* name) {
+    if (state_ptr) {
+        if (strings_equali(name, DEFAULT_CAMERA_NAME)) {
+            KTRACE("Cannot release default camera. Nothing was done.");
+            return;
+        }
+        u16 id = INVALID_ID_U16;
+        if (!hashtable_get(&state_ptr->lookup, name, &id)) {
+            KWARN("camera_system_release failed lookup. Nothing was done.");
+        }
+
+        if (id != INVALID_ID_U16) {
+            // Decrement the reference count, and reset the camera if the counter reaches 0.
+            state_ptr->cameras[id].reference_count--;
+            if (state_ptr->cameras[id].reference_count < 1) {
+                camera_reset(&state_ptr->cameras[id].c);
+                state_ptr->cameras[id].id = INVALID_ID_U16;
+                hashtable_set(&state_ptr->lookup, name, &state_ptr->cameras[id].id);
+            }
+        }
+    }
+}
+
+camera* camera_system_get_default() {
+    if (state_ptr) {
+        return &state_ptr->default_camera;
+    }
+
+    KERROR("camera_system_get_default called before system was initialized. Returned null.");
+    return 0;
+}
