10c0ea1bfb86a6ffbdaed55e44fd3086eb0f3723
Travis Vroman <travisvroman@gmail.com>
2022-05-23 20:32:55 -0400

    Resolves #59

--- engine/src/containers/freelist.c
+++ engine/src/containers/freelist.c
@@ -196,6 +196,13 @@ b8 freelist_resize(freelist* list, u64* memory_requirement, void* new_memory, u6
 
     // Enough space to hold state, plus array for all nodes.
     u64 max_entries = (new_size / sizeof(void*));  // NOTE: This might have a remainder, but that's ok.
+    
+    // Catch an edge case of having a really small amount of memory to manage, and only having a
+    // super small number of entries. Always make sure we have at least a decent amount, like 20 or so.
+    if (max_entries < 20) {
+        max_entries = 20;
+    }
+
     *memory_requirement = sizeof(internal_state) + (sizeof(freelist_node) * max_entries);
     if (!new_memory) {
         return true;
