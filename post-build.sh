0b243c52a0aabbd7885424b2c5a65151235950b3
Travis Vroman <travisvroman@gmail.com>
2022-07-02 22:35:30 -0400

    Fixed typo in post-build scripts

--- post-build.sh
+++ post-build.sh
@@ -9,8 +9,8 @@ pushd bin
 ../assets/shaders/Builtin.MaterialShader.frag.glsl \
 ../assets/shaders/Builtin.UIShader.vert.glsl \
 ../assets/shaders/Builtin.UIShader.frag.glsl \
-../assets/shaders/Builtin.UIShader.vert.glsl \
-../assets/shaders/Builtin.UIShader.frag.glsl \
+../assets/shaders/Builtin.SkyboxShader.vert.glsl \
+../assets/shaders/Builtin.SkyboxShader.frag.glsl \
 
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]
