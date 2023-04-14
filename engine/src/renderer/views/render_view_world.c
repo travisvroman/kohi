dfaddb0cdae923081d5e543423db6975d83fd19c
Travis Vroman <travisvroman@gmail.com>
2023-04-13 23:10:36 -0400

    WIP terrain work. Left off on terrain rendering hookup

--- engine/src/renderer/views/render_view_world.c
+++ engine/src/renderer/views/render_view_world.c
@@ -268,60 +268,59 @@ b8 render_view_world_on_render(const struct render_view* self, const struct rend
         for(u32 i = 0; i < terrain_count; ++i) {
 
             shader* s = shader_system_get("Builtin.TerrainShader");
-    if(!s) {
-        KERROR("Unable to obtain terrain shader.");
-        return false;
-    }
-    shader_system_use_by_id(s->id);
+            if(!s) {
+                KERROR("Unable to obtain terrain shader.");
+                return false;
+            }
+            shader_system_use_by_id(s->id);
 
-    // Apply uniforms, all are global for terrains.
-    shader_system_uniform_set_by_index(terrain_locations.projection, projection);
-    shader_system_uniform_set_by_index(terrain_locations.view, view);
-    shader_system_uniform_set_by_index(terrain_locations.ambient_colour, ambient_colour);
-    shader_system_uniform_set_by_index(terrain_locations.view_position, view_position);
-    shader_system_uniform_set_by_index(terrain_locations.render_mode, &render_mode);
+            // Apply uniforms, all are global for terrains.
+            shader_system_uniform_set_by_index(terrain_locations.projection, projection);
+            shader_system_uniform_set_by_index(terrain_locations.view, view);
+            shader_system_uniform_set_by_index(terrain_locations.ambient_colour, ambient_colour);
+            shader_system_uniform_set_by_index(terrain_locations.view_position, view_position);
+            shader_system_uniform_set_by_index(terrain_locations.render_mode, &render_mode);
 
     
-    // TODO: hardcoded temp
-    vec4 white = vec4_one();
-    shader_system_uniform_set_by_index(terrain_locations.diffuse_colour, &white);
-    //shader_system_uniform_set_by_index(state_ptr->material_locations.diffuse_texture, &m->diffuse_map);
-    //shader_system_uniform_set_by_index(state_ptr->material_locations.specular_texture, &m->specular_map);
-    //shader_system_uniform_set_by_index(state_ptr->material_locations.normal_texture, &m->normal_map);
-    // TODO: hardcoded temp
-    f32 shininess = 32.0f;
-    shader_system_uniform_set_by_index(terrain_locations.shininess, &shininess);
+            // TODO: hardcoded temp
+            vec4 white = vec4_one();
+            shader_system_uniform_set_by_index(terrain_locations.diffuse_colour, &white);
+            //shader_system_uniform_set_by_index(state_ptr->material_locations.diffuse_texture, &m->diffuse_map);
+            //shader_system_uniform_set_by_index(state_ptr->material_locations.specular_texture, &m->specular_map);
+            //shader_system_uniform_set_by_index(state_ptr->material_locations.normal_texture, &m->normal_map);
+            // TODO: hardcoded temp
+            f32 shininess = 32.0f;
+            shader_system_uniform_set_by_index(terrain_locations.shininess, &shininess);
 
-    // Directional light.
-    directional_light* dir_light = light_system_directional_light_get();
-    if (dir_light) {
-        shader_system_uniform_set_by_index(terrain_locations.dir_light, &dir_light->data);
-    } else {
-        directional_light_data data = {0};
-        shader_system_uniform_set_by_index(terrain_locations.dir_light, &data);
-    }
-    // Point lights.
-    u32 p_light_count = light_system_point_light_count();
-    if (p_light_count) {
-        // TODO: frame allocator?
-        point_light* p_lights = kallocate(sizeof(point_light) * p_light_count, MEMORY_TAG_ARRAY);
-        light_system_point_lights_get(p_lights);
+            // Directional light.
+            directional_light* dir_light = light_system_directional_light_get();
+            if (dir_light) {
+                shader_system_uniform_set_by_index(terrain_locations.dir_light, &dir_light->data);
+            } else {
+                directional_light_data data = {0};
+                shader_system_uniform_set_by_index(terrain_locations.dir_light, &data);
+            }
+            // Point lights.
+            u32 p_light_count = light_system_point_light_count();
+            if (p_light_count) {
+                // TODO: frame allocator?
+                point_light* p_lights = kallocate(sizeof(point_light) * p_light_count, MEMORY_TAG_ARRAY);
+                light_system_point_lights_get(p_lights);
 
-        point_light_data* p_light_datas = kallocate(sizeof(point_light_data) * p_light_count, MEMORY_TAG_ARRAY);
-        for (u32 i = 0; i < p_light_count; ++i) {
-            p_light_datas[i] = p_lights[i].data;
-        }
+                point_light_data* p_light_datas = kallocate(sizeof(point_light_data) * p_light_count, MEMORY_TAG_ARRAY);
+                for (u32 i = 0; i < p_light_count; ++i) {
+                    p_light_datas[i] = p_lights[i].data;
+                }
 
-        shader_system_uniform_set_by_index(terrain_locations.p_lights, p_light_datas);
-        kfree(p_light_datas, sizeof(point_light_data), MEMORY_TAG_ARRAY);
-        kfree(p_lights, sizeof(point_light), MEMORY_TAG_ARRAY);
-    }
+                shader_system_uniform_set_by_index(terrain_locations.p_lights, p_light_datas);
+                kfree(p_light_datas, sizeof(point_light_data), MEMORY_TAG_ARRAY);
+                kfree(p_lights, sizeof(point_light), MEMORY_TAG_ARRAY);
+            }
 
-    shader_system_uniform_set_by_index(terrain_locations.num_p_lights, &p_light_count);
-
-    shader_system_uniform_set_by_index(terrain_locations.model, &model);
-
-    shader_system_apply_global();
+            shader_system_uniform_set_by_index(terrain_locations.num_p_lights, &p_light_count);
+    
+            shader_system_uniform_set_by_index(terrain_locations.model, &model);    
+            shader_system_apply_global();
 
     
             // TODO: This isn't needed, use the one passed in instead
